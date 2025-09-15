1194e070d53c295ac2061f0a679585028d1d7ca6
Josh Cooper <josh@cooper.dev>
2025-09-15 13:08:39 +0900

    Show relative paths in statusline

--- nvim/lua/zen/statusline.lua
+++ nvim/lua/zen/statusline.lua
@@ -1,12 +1,48 @@
 -- TODO:
---   - relative paths under cwd
 --   - show file encoding and line endings if they aren't typical (utf-8, unix)
 --   - show buffer flags (e.g. dirty)
 
 local M = {}
 
-function M.statusline(info)
-	return "%f%=WIP"
+---@param buf integer
+---@return string
+local function buffer_name(buf)
+	local name = vim.api.nvim_buf_get_name(buf)
+
+	if name == "" then
+		return "[No Name]"
+	end
+
+	if vim.bo[buf].buftype == "help" then
+		return vim.fn.fnamemodify(name, ":t")
+	end
+
+	if vim.bo[buf].buftype == "terminal" then
+		return (name:gsub("^term://.-//[0-9]+:", "term://"))
+	end
+
+	if vim.bo[buf].buftype ~= "" then
+		return name
+	end
+
+	local cwd_prefix = ("%s/"):format(vim.fn.getcwd())
+
+	if vim.startswith(name, cwd_prefix) then
+		return name:sub(#cwd_prefix + 1)
+	end
+
+	return name
+end
+
+function M.statusline()
+	local win = vim.g.statusline_winid or vim.api.nvim_get_current_win()
+	local buf = vim.api.nvim_win_get_buf(win)
+
+	return table.concat({
+		buffer_name(buf),
+		"%=",
+		"WIP",
+	})
 end
 
 return M
