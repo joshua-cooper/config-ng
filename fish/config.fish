9c5fbd691e9d231ff54f4f4aa18347dae1b86db0
Josh Cooper <josh@cooper.dev>
2025-11-13 19:22:13 +0900

    Make escape with modifiers leave fish insert mode

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,22 @@
 set -g fish_greeting
 set -g fish_key_bindings fish_vi_key_bindings
+
+function fish_user_key_bindings
+    set modifiers super ctrl alt shift
+    set n (count $modifiers)
+
+    for mask in (seq (math "pow(2, $n) - 1"))
+        set combo
+
+        for i in (seq $n)
+            if test (math "bitand($mask, pow(2, $i - 1))") -ne 0
+                set combo $combo $modifiers[$i]
+            end
+        end
+
+        set combo $combo escape
+        set key (string join - $combo)
+
+        bind --mode insert --sets-mode default $key repaint-mode
+    end
+end
