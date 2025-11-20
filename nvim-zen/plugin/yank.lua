5fd862bc9e4eb91856d8b70d88dc8ee5ec6beeaf
Josh Cooper <josh@cooper.dev>
2025-11-20 12:02:05 +0700

    Add yank autocmd

---
+++ nvim-zen/plugin/yank.lua
@@ -0,0 +1,13 @@
+local group = vim.api.nvim_create_augroup("zen.yank", {})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = group,
+	desc = "Highlight text that was yanked",
+	callback = function(_)
+		vim.hl.on_yank({
+			higroup = "Visual",
+			timeout = 200,
+			on_visual = false,
+		})
+	end,
+})
