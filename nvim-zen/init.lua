0d5b51adb7a1ef6bddfcff27e0b11c6242745afe
Josh Cooper <josh@cooper.dev>
2025-11-20 12:04:31 +0700

    add exrc

--- nvim-zen/init.lua
+++ nvim-zen/init.lua
@@ -1,8 +1,9 @@
+vim.o.colorcolumn = "80"
+vim.o.exrc = true
 vim.o.splitbelow = true
 vim.o.splitright = true
 vim.o.undofile = true
 vim.o.wrap = false
-vim.o.colorcolumn = "80"
 
 vim.lsp.enable({
 	"zen/emmylua-ls",
