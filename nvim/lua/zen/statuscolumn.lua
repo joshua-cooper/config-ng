ecae0cf59805e5eb3f86b21d30c82fad2443ff43
Josh Cooper <josh@cooper.dev>
2025-12-07 20:55:16 +0700

    Fix statuscolumn window

--- nvim/lua/zen/statuscolumn.lua
+++ nvim/lua/zen/statuscolumn.lua
@@ -1,14 +1,34 @@
 local M = {}
 
+---@return integer?
+local function statusline_winid()
+	local winnr = vim.g.statusline_winid
+
+	if winnr == nil then
+		return nil
+	end
+
+	assert(
+		type(winnr) == "number" and math.floor(winnr) == winnr,
+		"vim.g.statusline_winid should be an integer"
+	)
+
+	return winnr ---@as integer
+end
+
 ---@return string
 function M.statuscolumn()
 	local s = ""
 
-	if vim.wo.foldcolumn ~= "0" then
+	local winnr = statusline_winid() or vim.api.nvim_get_current_win()
+	local has_foldcolumn = vim.wo[winnr].foldcolumn ~= "0"
+	local has_number = vim.wo[winnr].number or vim.wo[winnr].relativenumber
+
+	if has_foldcolumn then
 		s = s .. "%C "
 	end
 
-	if vim.wo.number or vim.wo.relativenumber then
+	if has_number then
 		s = s .. "%l "
 	end
 
