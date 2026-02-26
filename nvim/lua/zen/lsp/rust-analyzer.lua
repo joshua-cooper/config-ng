ce9e080ac0865eaf660bbce5f16fbafda9a6e7f5
Josh Cooper <josh@cooper.dev>
2026-02-26 15:09:58 +0700

    Lazy load rust-analyzer config

--- nvim/lua/zen/lsp/rust-analyzer.lua
+++ nvim/lua/zen/lsp/rust-analyzer.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+local CARGO_METADATA_COMMAND = {
+	"cargo",
+	"metadata",
+	"--no-deps",
+	"--format-version",
+	"1",
+}
+
 ---@param err lsp.ResponseError
 local function notify_server_error(err)
 	vim.notify(
@@ -10,6 +18,28 @@ local function notify_server_error(err)
 		),
 		vim.log.levels.ERROR
 	)
+end
+
+---@param stderr string
+local function notify_cargo_metadata_error(stderr)
+	vim.schedule(function()
+		vim.notify_once(
+			string.format(
+				"Got an error from `cargo metadata`:\n\n%s",
+				stderr
+			),
+			vim.log.levels.ERROR
+		)
+	end)
+end
+
+local function notify_no_workspace_root_from_cargo_metadata()
+	vim.schedule(function()
+		vim.notify_once(
+			"No workspace_root returned from `cargo metadata`",
+			vim.log.levels.WARN
+		)
+	end)
 end
 
 ---@return vim.lsp.Client?
@@ -88,6 +118,49 @@ local function broadcast_request(method, success_message)
 			vim.notify(success_message)
 		end)
 	end
+end
+
+---@param path string
+---@return boolean
+local function is_external_crate(path)
+	local join = vim.fs.joinpath
+
+	local user_home = assert(vim.uv.os_homedir())
+	local cargo_home = vim.env.CARGO_HOME or join(user_home, ".cargo")
+	local rustup_home = vim.env.RUSTUP_HOME or join(user_home, ".rustup")
+
+	local cargo_registry = join(cargo_home, "registry", "src")
+	local git_registry = join(cargo_home, "git", "checkouts")
+	local toolchains = join(rustup_home, "toolchains")
+	local nix_store = "/nix/store"
+
+	local dirs = {
+		cargo_registry,
+		git_registry,
+		toolchains,
+		nix_store,
+	}
+
+	for _, dir in ipairs(dirs) do
+		if vim.fs.relpath(dir, path) then
+			return true
+		end
+	end
+
+	return false
+end
+
+---@param folders lsp.WorkspaceFolder[]
+---@param path string
+---@return boolean
+local function has_workspace_folder(folders, path)
+	for _, folder in ipairs(folders) do
+		if folder.name == path then
+			return true
+		end
+	end
+
+	return false
 end
 
 ---@param command lsp.Command
@@ -267,6 +340,157 @@ function M.external_docs()
 
 		vim.ui.open(result)
 	end)
+end
+
+---@param params lsp.InitializeParams
+---@param config vim.lsp.ClientConfig
+function M.before_init(params, config)
+	if config.settings and config.settings["rust-analyzer"] then
+		params.initializationOptions = config.settings["rust-analyzer"]
+	end
+end
+
+---@param bufnr integer
+---@param on_dir fun(root_dir?: string)
+function M.root_dir(bufnr, on_dir)
+	local crate_dir = vim.fs.root(bufnr, "Cargo.toml")
+
+	if not crate_dir then
+		local rust_project_dir = vim.fs.root(bufnr, "rust-project.json")
+
+		if rust_project_dir then
+			on_dir(rust_project_dir)
+		end
+
+		return
+	end
+
+	if vim.fn.executable(CARGO_METADATA_COMMAND[1]) == 0 then
+		on_dir(crate_dir)
+		return
+	end
+
+	local opts = {
+		text = true,
+		cwd = crate_dir,
+		env = {
+			-- Allow cargo to parse manifests with unstable
+			-- features.
+			RUSTC_BOOTSTRAP = "1",
+		},
+	}
+
+	vim.system(CARGO_METADATA_COMMAND, opts, function(result)
+		if result.code ~= 0 then
+			notify_cargo_metadata_error(assert(result.stderr))
+			on_dir(crate_dir)
+			return
+		end
+
+		local output = vim.json.decode(assert(result.stdout))
+		local workspace_root = output.workspace_root
+
+		if type(workspace_root) ~= "string" then
+			notify_no_workspace_root_from_cargo_metadata()
+			on_dir(crate_dir)
+			return
+		end
+
+		on_dir(vim.fs.normalize(workspace_root))
+	end)
+end
+
+---@param client vim.lsp.Client
+---@param config vim.lsp.ClientConfig
+---@return boolean
+function M.reuse_client(client, config)
+	if client.name ~= config.name then
+		return false
+	end
+
+	if client.root_dir == config.root_dir then
+		return true
+	end
+
+	if not config.root_dir then
+		return false
+	end
+
+	if is_external_crate(config.root_dir) then
+		return true
+	end
+
+	client.workspace_folders = client.workspace_folders or {}
+
+	local should_add_to_workspace = not has_workspace_folder(
+		client.workspace_folders,
+		config.root_dir
+	)
+
+	if should_add_to_workspace then
+		local folder = {
+			uri = vim.uri_from_fname(config.root_dir),
+			name = config.root_dir,
+		}
+
+		client:notify("workspace/didChangeWorkspaceFolders", {
+			event = {
+				added = {
+					folder,
+				},
+				removed = {},
+			},
+		})
+
+		client.workspace_folders[#client.workspace_folders + 1] = folder
+	end
+
+	return true
+end
+
+---@param client vim.lsp.Client
+---@param bufnr integer
+function M.on_attach(client, bufnr)
+	local USER_COMMANDS = {
+		LspRustAnalyzerExpandMacro = M.expand_macro,
+		LspRustAnalyzerViewMir = M.view_mir,
+		LspRustAnalyzerViewHir = M.view_hir,
+		LspRustAnalyzerReloadWorkspace = M.reload_workspace,
+		LspRustAnalyzerRebuildProcMacros = M.rebuild_proc_macros,
+		LspRustAnalyzerOpenCargoToml = M.open_cargo_toml,
+		LspRustAnalyzerParentModule = M.parent_module,
+		LspRustAnalyzerExternalDocs = M.external_docs,
+	}
+
+	for name, command in pairs(USER_COMMANDS) do
+		vim.api.nvim_buf_create_user_command(bufnr, name, command, {})
+	end
+
+	local group = vim.api.nvim_create_augroup(
+		string.format("zen.lsp.%s:%d", client.name, bufnr),
+		{}
+	)
+
+	vim.api.nvim_create_autocmd("LspDetach", {
+		group = group,
+		buffer = bufnr,
+		desc = string.format("Clean up %s LSP clients", client.name),
+		callback = function(args)
+			if args.data.client_id ~= client.id then
+				return
+			end
+
+			for name, _ in pairs(USER_COMMANDS) do
+				pcall(
+					vim.api.nvim_buf_del_user_command,
+					bufnr,
+					name
+				)
+			end
+
+			return true
+		end,
+	})
 end
 
 return M
